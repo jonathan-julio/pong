13d7d5da699a5a8db4be00b8956b3333ead7e32b
jonathan-julio <jonathan.goncalves.703@ufrn.edu.br>
2023-05-25 23:58:21 -0300

    correção fisica do jogo

--- public/socket.js
+++ public/socket.js
@@ -1,6 +1,5 @@
-
-var initGame = false;
 const socket = io();
+var initGame = false;
 const canvas = document.getElementById('canvas-jogo');
 const context = canvas.getContext('2d');
 
@@ -30,231 +29,159 @@ const canvasPlacar = document.getElementById('canvas-placar');
 const contextPlacar = canvasPlacar.getContext('2d');
 const pontuacaoElement = document.getElementById('pontuacao');
 
-
-
-socket.on('connect', function() {
-    console.log('Conectado ao servidor');
+socket.on('connect', function () {
+  console.log('Conectado ao servidor:', socket.id);
 });
 
-socket.on('disconnect', function() {
-    console.log('Desconectado do servidor');
-    document.getElementById('game').style.display = 'none';
-    document.getElementById('load').style.display = 'block';
+socket.on('disconnect', function () {
+  console.log('Desconectado do servidor:', socket.id);
+  document.getElementById('game').style.display = 'none';
+  document.getElementById('load').style.display = 'block';
 });
 
-socket.on('ballPosition', function(data) {
-    bolaX = data.x;
-    bolaY = data.y;
-  });
-  
-socket.on('startGame', function(msg) {
-    var mensagem = "";
-    console.log("if : " +msg['message'] === "Jogo iniciou" && document.getElementById('load').style.display !== 'none');
-    if (msg['message'] === "Jogo iniciou" && document.getElementById('load').style.display !== 'none') {
-        mensagem = "Jogo iniciou s";
-    }else{
-        mensagem = "O jogo reiniciou";
-    }
-    showDialog(mensagem).then((confirmed) => {
-        if (confirmed) {
-          socket.emit('initGame', {status : true});
-        } else {
-          socket.emit('initGame', {status : false});
-        }
-      });
-    myPlayerString = msg['role'];
-    if (msg['role'] == "player2") {
-        ladoOponente = (bolaX - ballRadius < 0 );
-        meuLado= (bolaX + ballRadius > canvas.width);
-        myPlayer = canvas.width - larguraRaquete - 2;
-        oponentePlayer = 5;
-    } else {
-        meuLado = (bolaX - ballRadius < 0 );
-        ladoOponente = (bolaX + ballRadius > canvas.width);
-        oponentePlayer = canvas.width - larguraRaquete - 2;
-        myPlayer = 5;
-    }
-    playerY = 50;
-    opponentY = 50;
-    document.getElementById('load').style.display = 'none';
-    document.getElementById('game').style.display = 'block';
-    //alert(msg);
-});
-
-socket.on('waiting', function(msg) {
-    document.getElementById('game').style.display = 'none';
-    document.getElementById('load').style.display = 'block';
-    alert(msg);
-});
-
-socket.on('update', function(msg) {
-    opponentY = msg['oponent']
+socket.on('gameFound', function (data) {
+  console.log('Jogo encontrado. Oponente ID:', data.opponentId);
 });
 
 socket.on('init', function(data) {
-    initGame = data;
+  initGame = data;
 });
 
-socket.on('pontos', function(data) {
-    console.log("ponto : " + data.pontoPlay1);
-    console.log("ponto2 : " + data.pontoPlay2);
-    pontoPlay1 = data["pontoPlay1"];
-    pontoPlay2 = data["pontoPlay2"];
-    atualizarPlacar();
-});
-
-function showDialog(message) {
-    return new Promise((resolve) => {
-      const result = confirm(message);
-      resolve(result);
+socket.on('startGame', function(msg) {
+  var mensagem = "";
+  if (msg['message'] === "Jogo iniciou" && document.getElementById('load').style.display !== 'none') {
+      mensagem = "Jogo iniciou s";
+  }else{
+      mensagem = "O jogo reiniciou";
+  }
+  showDialog(mensagem).then((confirmed) => {
+      if (confirmed) {
+        socket.emit('initGame', {status : true, canvasWidth : canvas.width , canvasHeight : canvas.height});
+      } else {
+        socket.emit('initGame', {status : false, canvasWidth : canvas.width , canvasHeight : canvas.height});
+      }
     });
-}
-  
+  myPlayerString = msg['role'];
+  if (msg['role'] == "player2") {
+      ladoOponente = (bolaX - ballRadius < 0 );
+      meuLado= (bolaX + ballRadius > canvas.width);
+      myPlayer = canvas.width - larguraRaquete - 1;
+      oponentePlayer = 1;
+  } else {
+      meuLado = (bolaX - ballRadius < 0 );
+      ladoOponente = (bolaX + ballRadius > canvas.width);
+      oponentePlayer = canvas.width - larguraRaquete - 1;
+      myPlayer = 1;
+  }
+  playerY = 50;
+  opponentY = 50;
+  document.getElementById('load').style.display = 'none';
+  document.getElementById('game').style.display = 'block';
+});
 
-function atualizarPlacar() {
-    contextPlacar.clearRect(0, 0, canvasPlacar.width, canvasPlacar.height);
+socket.on('ballPosition', function (data) {
+  /* console.log('Nova posição da bola:', data.x, data.y); */
+  bolaX = data.x;
+  bolaY = data.y;
+});
+
+socket.on('update', function (data) {
+  opponentY = data.oponent;
+});
+
+socket.on('pontos', function (data) {
+  console.log('Atualização da pontuação:', data.pontoPlay1, data.pontoPlay2);
+  
+  if (pontoPlay1 != data.pontoPlay1 || pontoPlay2 != data.pontoPlay2) {
+    console.log("ponto para play1")
+  }
+  if (pontoPlay2 != data.pontoPlay2) {
+    console.log("ponto para play2")
+  }
+  pontoPlay1 = data.pontoPlay1;
+  pontoPlay2 = data.pontoPlay2;
+  contextPlacar.clearRect(0, 0, canvasPlacar.width, canvasPlacar.height);
     contextPlacar.font = '20px Arial';
     contextPlacar.fillText(`Pontuação: ${pontoPlay1} x ${pontoPlay2} `, 10, 30);
     pontuacaoElement.textContent = `Pontuação: ${pontoPlay1} x ${pontoPlay2}`;
+});
+
+
+
+function sendPlayerPosition() {
+  if (playerMoveUp && playerY > 0) {
+    playerY -= 5;
+  } else if (playerMoveDown && playerY + alturaRaquete < canvas.height) {
+      playerY += 5;
+  }
+  socket.emit('paddlePosition', { playerY : playerY });
 }
-  
 
-  function desenharRaquetes() {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      context.fillStyle = 'red';
-      context.fillRect(myPlayer, playerY, larguraRaquete, alturaRaquete);
-      context.fillStyle = 'black';
-      context.fillRect(oponentePlayer, opponentY, larguraRaquete, alturaRaquete);
-      if (initGame) {
-        updateBall ();
-        sendBallPosition();
-      }
-  }
-  
-  function updateBall() {
-      bolaX += velocidadeBolaX;
-      bolaY += velocidadeBolaY;
-    
-      // Lógica de colisão com as paredes verticais
-      if (bolaY + ballRadius > canvas.height || bolaY - ballRadius < 0) {
-        velocidadeBolaY = -velocidadeBolaY;
-      }
-    
-      // Lógica de colisão com as raquetes
-      if (
-        (bolaX - ballRadius < larguraRaquete && bolaY + ballRadius > playerY && bolaY - ballRadius < playerY + alturaRaquete) ||
-        (bolaX + ballRadius > canvas.width - larguraRaquete && bolaY + ballRadius > opponentY && bolaY - ballRadius < opponentY + alturaRaquete)
-      ) {
-        velocidadeBolaX = -velocidadeBolaX;
-      }
-    
-      // Lógica de pontuação quando a bola ultrapassa as raquetes
-      if (bolaX - ballRadius < 0 ) {
-        pontoPlay2 += 1;
-        socket.emit('initGame', {status : false});
-        showDialog("Ponto para o Player 2. Deseja continuar?").then((confirmed) => {
-            if (confirmed) {
-              socket.emit('initGame', {status : true});
-            } else {
-              socket.emit('initGame', {status : false});
-            }
-        });
-        bolaX = canvas.width / 2; 
-        bolaY = canvas.height / 2; 
-        sendBallPosition();
-        
-      } else if (bolaX + ballRadius > canvas.width) {
-        pontoPlay1 += 1;
-        socket.emit('initGame', {status : false});
-        showDialog("Ponto para o Player 1. Deseja continuar?").then((confirmed) => {
-            if (confirmed) {
-              socket.emit('initGame', {status : true});
-            } else {
-              socket.emit('initGame', {status : false});
-            }
-        });
-        bolaX = canvas.width / 2; 
-        bolaY = canvas.height / 2;
+function sendBallPosition(x, y) {
+  socket.emit('ballPosition', { x: x, y: y });
+}
 
-        sendBallPosition();
-      }
-      if (myPlayerString == "player1") {
-        socket.emit("marcarPonto" , {"pontoPlay1": pontoPlay1, "pontoPlay2" : pontoPlay2});
-      }
-    
-      // Verificar limites horizontais
-      if (bolaX - ballRadius < 0) {
-        bolaX = ballRadius;
-      } else if (bolaX + ballRadius > canvas.width) {
-        bolaX = canvas.width - ballRadius;
-      }
-    
-      // Verificar limites verticais
-      if (bolaY - ballRadius < 0) {
-        bolaY = ballRadius;
-      } else if (bolaY + ballRadius > canvas.height) {
-        bolaY = canvas.height - ballRadius;
-      }
-    
-      // Desenhar a bola
-      context.beginPath();
-      context.arc(bolaX, bolaY, ballRadius, 0, Math.PI * 2, false);
-      context.fillStyle = 'green'; 
-      context.fill();
-      context.closePath();
-    }
-    
-  
-  // Função para atualizar a tela
-  function updateScreen() {
-      // Movimento do jogador
-      if (playerMoveUp && playerY > 0) {
-          playerY -= 5;
-      } else if (playerMoveDown && playerY + alturaRaquete < canvas.height) {
-          playerY += 5;
-      }
-  
-      // Desenhar as raquetes
-      desenharRaquetes();
-  
-      // Chamar a próxima atualização da tela
-      requestAnimationFrame(updateScreen);
+function sendScoreUpdate(player1Score, player2Score) {
+  socket.emit('scoreUpdate', { player1: player1Score, player2: player2Score });
+}
+
+function resetBall() {
+  bolaX = canvas.width / 2;
+  bolaY = canvas.height / 2;
+  velocidadeBolaX *= -1;
+  velocidadeBolaY *= -1;
+}
+
+
+function desenharBola() {
+  context.beginPath();
+  context.arc(bolaX, bolaY, ballRadius, 0, Math.PI * 2, false);
+  context.fillStyle = 'green';
+  context.fill();
+  context.closePath();
+}
+function desenharRaquetes() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = 'red';
+  context.fillRect(myPlayer, playerY, larguraRaquete, alturaRaquete);
+  context.fillStyle = 'black';
+  context.fillRect(oponentePlayer, opponentY, larguraRaquete, alturaRaquete);
+}
+
+
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'ArrowUp') {
+      playerMoveUp = true;
+      sendPlayerPosition();
+  } else if (event.key === 'ArrowDown') {
+      playerMoveDown = true;
+      sendPlayerPosition();
   }
-  
-  function sendPaddlePosition() {
-      const data = {playerY: playerY};
-      socket.emit('paddlePosition', data);
+});
+
+// Evento de soltar a tecla
+document.addEventListener('keyup', function(event) {
+  if (event.key === 'ArrowUp') {
+      playerMoveUp = false;
+      sendPlayerPosition();
+  } else if (event.key === 'ArrowDown') {
+      playerMoveDown = false;
+      sendPlayerPosition();
   }
-  
-   // Função para enviar a posição da bola para o servidor
-   function sendBallPosition() {
-      const data = { x: bolaX, y: bolaY };
-      socket.emit('ballPosition', data);
-  }
-  
-  
-  // Evento de pressionar a tecla
-  document.addEventListener('keydown', function(event) {
-      if (event.key === 'ArrowUp') {
-          playerMoveUp = true;
-          sendPaddlePosition();
-      } else if (event.key === 'ArrowDown') {
-          playerMoveDown = true;
-          sendPaddlePosition();
-      }
+});
+
+function gameLoop() {
+    desenharRaquetes();
+    desenharBola();
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
+
+function showDialog(message) {
+  return new Promise((resolve) => {
+    const result = confirm(message);
+    resolve(result);
   });
-  
-  // Evento de soltar a tecla
-  document.addEventListener('keyup', function(event) {
-      if (event.key === 'ArrowUp') {
-          playerMoveUp = false;
-          sendPaddlePosition();
-      } else if (event.key === 'ArrowDown') {
-          playerMoveDown = false;
-          sendPaddlePosition();
-      }
-  });
-  
-  
-  // Chamar a função inicial de atualização da tela
-  updateScreen();
+}
--- socket.js
+++ socket.js
@@ -1,27 +1,34 @@
-const app = require('express')()
+const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-
 app.get('/style.css', (req, res) => {
   res.sendFile(__dirname +'/public/style.css');
-})
+});
+
 app.get('/socket.js', (req, res) => {
   res.sendFile(__dirname +'/public/socket.js');
-})
+});
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname +'/public/index.html');
-})
+  res.sendFile(__dirname +'/public/index.html');
+});
 
-
-
-
-
-// armazena as informações dos jogadores em espera e em jogo
 let jogadoresEspera = [];
 let jogos = [];
 let jogadores = [];
+
+
+
+
+
+class canvas {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+}
+
 
 
 io.on('connection', (socket) => {
@@ -32,13 +39,13 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     for (var i = 0; i < jogos.length; i++) {
-      var objeto = jogos[i] ;
-      if (objeto.socketId1 === socket.id  || objeto.socketId2 === socket.id) {
+      var objeto = jogos[i];
+      if (objeto.socketId1 === socket.id || objeto.socketId2 === socket.id) {
         var socketID;
-        if (objeto.socketId1 === socket.id){
+        if (objeto.socketId1 === socket.id) {
           socketID = objeto.socketId2;
           jogadoresEspera.push(objeto.socketId2);
-        } else{
+        } else {
           socketID = objeto.socketId1;
           jogadoresEspera.push(objeto.socketId1);
         } 
@@ -47,7 +54,7 @@ io.on('connection', (socket) => {
     }
     for (var i = 0; i < jogadoresEspera.length; i++) {
       var sockeyID = jogadoresEspera[i];
-      if (sockeyID === socket.id ) {
+      if (sockeyID === socket.id) {
         jogadoresEspera.splice(i, 1);
       }
     }
@@ -55,39 +62,37 @@ io.on('connection', (socket) => {
     console.log('Usuário desconectado:', socket.id);
   });
 
-  // Quando um cliente envia a posição da bola para o servidor
-  // Atualiza a posição da bola e emite o evento para todos os clientes
   socket.on('ballPosition', function(data) {
     const playerSocketId = socket.id;
     ballX = data.x;
     ballY = data.y;
     const game = jogos.find(objeto => objeto.socketId1 === playerSocketId || objeto.socketId2 === playerSocketId);
     if (game) {
-      const socketPlay1 = jogadores.find(jogador => jogador.id === game.socketId1 );
-    const socketPlay2 = jogadores.find(jogador => jogador.id === game.socketId2 );
-    socketPlay1.emit('ballPosition', { x: ballX, y: ballY });
-    socketPlay2.emit('ballPosition', { x: ballX, y: ballY });
+      const socketPlay1 = jogadores.find(jogador => jogador.id === game.socketId1);
+      const socketPlay2 = jogadores.find(jogador => jogador.id === game.socketId2);
+      socketPlay1.emit('ballPosition', { x: ballX, y: ballY });
+      socketPlay2.emit('ballPosition', { x: ballX, y: ballY });
     }
   });
 
   socket.on('initGame', function(data) {
     const playerSocketId = socket.id;
     const game = jogos.find(objeto => objeto.socketId1 === playerSocketId || objeto.socketId2 === playerSocketId);
-
+    canvas.width = data['canvasWidth'];
+    canvas.height = data['canvasHeight'];
     if (game) {
-      if(game.socketId1 === playerSocketId ){
+      if (game.socketId1 === playerSocketId) {
         game.statusPlay1 = data["status"];
       }
-      if(game.socketId2 === playerSocketId){
+      if (game.socketId2 === playerSocketId) {
         game.statusPlay2 = data["status"];
       }
 
-      const socketPlay1 = jogadores.find(jogador => jogador.id === game.socketId1 );
-      const socketPlay2 = jogadores.find(jogador => jogador.id === game.socketId2 );
+      const socketPlay1 = jogadores.find(jogador => jogador.id === game.socketId1);
+      const socketPlay2 = jogadores.find(jogador => jogador.id === game.socketId2);
       socketPlay1.emit("init", (game.statusPlay1 && game.statusPlay2));
       socketPlay2.emit("init", (game.statusPlay1 && game.statusPlay2));
     }
-
   });
 
   socket.on('marcarPonto', function(data) {
@@ -95,27 +100,53 @@ io.on('connection', (socket) => {
     const game = jogos.find(objeto => objeto.socketId1 === playerSocketId || objeto.socketId2 === playerSocketId);
 
     if (game) {
-        game.pontoPlay1 = data.pontoPlay1;
-        game.pontoPlay2 = data.pontoPlay2;
+      game.pontoPlay1 = data.pontoPlay1;
+      game.pontoPlay2 = data.pontoPlay2;
       
-      const socketPlay1 = jogadores.find(jogador => jogador.id === game.socketId1 );
-      const socketPlay2 = jogadores.find(jogador => jogador.id === game.socketId2 );
-      socketPlay1.emit("pontos", {"pontoPlay1" : game.pontoPlay1, "pontoPlay2" : game.pontoPlay2});
-      socketPlay2.emit("pontos", {"pontoPlay1" : game.pontoPlay1, "pontoPlay2" : game.pontoPlay2});
+      const socketPlay1 = jogadores.find(jogador => jogador.id === game.socketId1);
+      const socketPlay2 = jogadores.find(jogador => jogador.id === game.socketId2);
+      socketPlay1.emit("pontos", { "pontoPlay1" : game.pontoPlay1, "pontoPlay2" : game.pontoPlay2 });
+      socketPlay2.emit("pontos", { "pontoPlay1" : game.pontoPlay1, "pontoPlay2" : game.pontoPlay2 });
     }
-
   });
 
   socket.on('paddlePosition', function(data) {
     const playerSocketId = socket.id;
-    var player1;
-    var player2;
+    var _player1;
+    var _player2;
+  
+    const game = jogos.find(objeto => objeto.socketId1 === playerSocketId || objeto.socketId2 === playerSocketId);
+    if (game) {
+      _player1 = game.socketId1;
+      _player2 = game.socketId2;
+
+      if (game.socketId1 === playerSocketId) {
+        game.raquetePlay1 = data.playerY;
+      } else {
+        game.raquetePlay2 = data.playerY;
+      }
+    }
+  
+    const socketPlay1 = jogadores.find(jogador => jogador.id === _player1);
+    const socketPlay2 = jogadores.find(jogador => jogador.id === _player2);
+    if (_player1 == playerSocketId) {
+      socketPlay2.emit("update", { "oponent" : game.raquetePlay1 });
+    } else {
+      socketPlay1.emit("update", { "oponent" : game.raquetePlay2 });
+    }    
+  });
+
+  socket.on('playerPosition', function(data) {
+
+    const playerSocketId = socket.id;
+    var _player1;
+    var _player2;
   
     // Encontrar o jogo correspondente ao socket do jogador
     const game = jogos.find(objeto => objeto.socketId1 === playerSocketId || objeto.socketId2 === playerSocketId);
     if (game) {
-      player1 = game.socketId1;
-      player2 = game.socketId2;
+      _player1 = game.socketId1;
+      _player2 = game.socketId2;
       // Atualizar a posição da raquete do jogador
       if (game.socketId1 === playerSocketId) {
         game.raquetePlay1 = data.playerY;
@@ -124,9 +155,9 @@ io.on('connection', (socket) => {
       }
     }
   
-    const socketPlay1 = jogadores.find(jogador => jogador.id === player1 );
-    const socketPlay2 = jogadores.find(jogador => jogador.id === player2 );
-    if (player1 == playerSocketId) {
+    const socketPlay1 = jogadores.find(jogador => jogador.id === _player1 );
+    const socketPlay2 = jogadores.find(jogador => jogador.id === _player2 );
+    if (_player1 == playerSocketId) {
       socketPlay2.emit("update", 
       { "oponent" : game.raquetePlay1}
     );
@@ -137,64 +168,156 @@ io.on('connection', (socket) => {
       
     }    
   });
-})
-
-
-
+});
 
 http.listen(3000, function() {
   console.log('Servidor ouvindo na porta 3000');
-})
+});
 
+const ballRadius = 5;
+const larguraRaquete = 2; 
+const alturaRaquete = 40; 
 
 class ObjetoComDoisSockets {
-  constructor(socket1, socket2, raquetePlay1, raquetePlay2,statusPlay1,statusPlay2, pontoPlay1, pontoPlay2) {
-    this.socketId1 = socket1;
-    this.socketId2 = socket2;
+  constructor(socketId1, socketId2, raquetePlay1, raquetePlay2, statusPlay1, statusPlay2, pontoPlay1, pontoPlay2, ballPositionX, ballPositionY,ballSpeedX,ballSpeedY) {
+    this.socketId1 = socketId1;
+    this.socketId2 = socketId2;
     this.raquetePlay1 = raquetePlay1;
     this.raquetePlay2 = raquetePlay2;
     this.statusPlay1 = statusPlay1;
     this.statusPlay2 = statusPlay2;
     this.pontoPlay1 = pontoPlay1;
     this.pontoPlay2 = pontoPlay2;
+    this.ballPositionX = ballPositionX;
+    this.ballPositionY = ballPositionY;
+   this.ballSpeedX =  ballSpeedX;
+   this.ballSpeedY =  ballSpeedY;
   }
 }
 
-function checkListEspera(){
+function checkListEspera() {
   if (jogadoresEspera.length >= 2) {
-    var player1 = jogadoresEspera[0];
-    var player2 = jogadoresEspera[1];
-    var obj = new ObjetoComDoisSockets(player1, player2, 5, 5, false, false, 0, 0);
+    var _player1 = jogadoresEspera[0];
+    var _player2 = jogadoresEspera[1];
+    var obj = new ObjetoComDoisSockets(_player1, _player2, 50, 50, false, false, 0, 0, canvas.width / 2, canvas.height / 2, 1, 1.2);
     jogos.push(obj);
     jogadoresEspera.splice(1, 1);
     jogadoresEspera.splice(0, 1);
     console.log('jogo iniciado');
-    const socket1 = jogadores.find(socket => socket.id === player1);
-    const socket2 = jogadores.find(socket => socket.id === player2);
+    const socket1 = jogadores.find(socket => socket.id === _player1);
+    const socket2 = jogadores.find(socket => socket.id === _player2);
     if (socket1 && socket2) {
-
-      // Emitir evento 'startGame' para cada jogador com o papel atribuído
-      socket1.emit('startGame', {
-        message: 'Jogo iniciou',
-        role: 'player1',
-      });
-
-      socket2.emit('startGame', {
-        message: 'Jogo iniciou',
-        role: 'player2',
-      });
-      console.log("testando ---------------- "+ player1);
-    console.log("testando ---------------- "+ player2)
+      socket1.emit('startGame', { message: 'Jogo iniciou', role: 'player1' });
+      socket2.emit('startGame', { message: 'Jogo iniciou', role: 'player2' });
     }
-    
-    
   } else {
-    var player1 = jogadoresEspera[0];
-    const socket = jogadores.find(socket => socket.id === player1);
+    var _player1 = jogadoresEspera[0];
+    const socket = jogadores.find(socket => socket.id === _player1);
     if (socket) {
-      socket.emit('waiting', 'Jogador foi pra fila de espera.');
+      socket.emit('waiting', 'Jogador foi para a fila de espera.');
     }
-    
   }
   console.log('lista espera: ', jogadoresEspera.length);
 }
+
+function moveBall() {
+  if(canvas.width == undefined || canvas.height == undefined){
+    canvas.height = 150;
+    canvas.width = 300
+  }
+  for (var i = 0; i < jogos.length; i++) {
+    var game = jogos[i];
+    
+    const socketPlay1 = jogadores.find(jogador => jogador.id === game.socketId1);
+    const socketPlay2 = jogadores.find(jogador => jogador.id === game.socketId2);
+    game.ballPositionX = game.ballPositionX + game.ballSpeedX;
+    game.ballPositionY = game.ballPositionY + game.ballSpeedY;
+
+    
+
+
+
+    if (game.ballPositionX === undefined || game.ballPositionY === undefined) {
+      game.ballPositionX = canvas.width / 2;
+      game.ballPositionY = canvas.height / 2;
+      console.log("speedX a : " + game.ballPositionX);
+      console.log("speedY  a: " + game.ballPositionY);
+    }
+    console.log("ballPositionX : " + game.ballSpeedX);
+
+
+    // Lógica de colisão com as paredes verticais
+    if (game.ballPositionY + ballRadius > canvas.height || game.ballPositionY - ballRadius < 0) {
+      game.ballSpeedY = -game.ballSpeedY;
+    }
+
+    // Lógica de colisão com as raquetes
+    if (
+      (game.ballPositionX - ballRadius < larguraRaquete && game.ballPositionY + ballRadius > game.raquetePlay1 && game.ballPositionY - ballRadius < game.raquetePlay1 + alturaRaquete) ||
+      (game.ballPositionX + ballRadius > canvas.width - larguraRaquete && game.ballPositionY + ballRadius > game.raquetePlay2 && game.ballPositionY - ballRadius < game.raquetePlay2 + alturaRaquete)
+    ) {
+      game.ballSpeedX = -game.ballSpeedX;
+      
+      if (game.ballSpeedX < 3) {
+        game.ballSpeedX = game.ballSpeedX * 1.15;
+        game.ballSpeedY = game.ballSpeedY * 1.15;
+        console.log("ponto para o player 2")
+      }
+      
+    }
+
+     // Lógica de pontuação quando a bola ultrapassa as raquetes
+     if (game.ballPositionX - ballRadius < 0 ) {
+      game.pontoPlay2 += 1;
+      console.log("ponto para o player 2");
+      game.ballPositionX = canvas.width / 2; 
+      game.ballPositionY = canvas.height / 2; 
+      
+    } else if (game.ballPositionX + ballRadius > canvas.width) {
+      game.pontoPlay1 += 1;
+      console.log("ponto para o player 1")
+      game.ballPositionX = canvas.width / 2; 
+      game.ballPositionY = canvas.height / 2;
+
+    }
+
+    if (game.ballPositionX - ballRadius < 0) {
+      game.ballPositionX = ballRadius;
+    } else if (game.ballPositionX + ballRadius > canvas.width) {
+      game.ballPositionX = canvas.width - ballRadius;
+    }
+  
+    // Verificar limites verticais
+    if (game.ballPositionY - ballRadius < 0) {
+      game.ballPositionY = ballRadius;
+    } else if (game.ballPositionY + ballRadius > canvas.height) {
+      game.ballPositionY = canvas.height - ballRadius;
+    }
+
+    socketPlay1.emit('ballPosition', { x: game.ballPositionX, y: game.ballPositionY });
+    socketPlay2.emit('ballPosition', { x: game.ballPositionX, y: game.ballPositionY });
+
+    socketPlay1.emit('pontos', { pontoPlay1: game.pontoPlay1, pontoPlay2: game.pontoPlay2 });
+    socketPlay2.emit('pontos', { pontoPlay1: game.pontoPlay1, pontoPlay2: game.pontoPlay2 });
+  
+    }
+   
+}
+
+function notifyPoint(game, player) {
+  const socketPlay1 = jogadores.find(jogador => jogador.id === game.socketId1);
+  const socketPlay2 = jogadores.find(jogador => jogador.id === game.socketId2);
+
+  if (player === 1) {
+    socketPlay1.emit('ponto', { jogador: 1 });
+    socketPlay2.emit('ponto', { jogador: 1 });
+  } else if (player === 2) {
+    socketPlay1.emit('ponto', { jogador: 2 });
+    socketPlay2.emit('ponto', { jogador: 2 });
+  }
+}
+
+
+
+
+setInterval(moveBall, 1000 / 60); // Move a bola a cada 1/60 segundos (60 FPS)
